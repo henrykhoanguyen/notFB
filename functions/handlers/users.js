d0be4db7aa8133a2811951b68795275e02bdc38e
Khoa Nguyen <henrykhoanguyen@gmail.com>
2019-07-23 16:02:23 -0700

    working on users backend

--- functions/handlers/users.js
+++ functions/handlers/users.js
@@ -1,11 +1,12 @@
-const { db } = require('../util/admin');
-
+const { admin, db } = require('../util/admin');
 const config = require('../util/config');
-const { validateSignupData, validateLoginData } = require('../util/validators');
+
+const { validateSignupData, validateLoginData, reduceUserDetails } = require('../util/validators');
 
 const firebase = require('firebase');
 firebase.initializeApp(config);
 
+// Signup for Users
 exports.signup = (req, res) => {
     const newUser = {
         email: req.body.email,
@@ -20,7 +21,9 @@ exports.signup = (req, res) => {
         return res.status(400).json(errors);
     }
 
-    let {token, userId} = 0
+    const noImg = 'no-img.png';
+
+    let {token, userId} = 0;
     db.doc(`/user/${newUser.handle}`).get()
         .then(doc => {
             if(doc.exists){
@@ -39,11 +42,12 @@ exports.signup = (req, res) => {
                 handle: newUser.handle,
                 email: newUser.email,
                 createdAt: new Date().toISOString(),
+                imageUrl: `https://firebasestorage.googleapis.com/v0/b/${config.storageBucket}/o/${noImg}?alt=media`,
                 userId
             };
             db.doc(`/users/${newUser.handle}`).set(userCredentials); // .set which create the document
         })
-        .then(data => {
+        .then(() => {
             return res.status(201).json({ token });
         })
         .catch(err => {
@@ -56,6 +60,7 @@ exports.signup = (req, res) => {
         });
 };
 
+// Login for User
 exports.login = (req, res) => {
     const user = {
         email: req.body.email,
@@ -84,4 +89,106 @@ exports.login = (req, res) => {
             }
             return res.status(500).json({ error: err.code });
         });
+};
+
+// Add User Info
+exports.addUserDetails = (req, res) => {
+    let userDetails = reduceUserDetails(req.body);
+
+    db.doc(`/users/${req.user.handle}`)
+        .update(userDetails)
+        .then(() => {
+            return res.json({ message: 'Info added successfully'});
+        })
+        .catch(err => {
+            console.error(err);
+            return res.status(500).json({ error: error.code });
+        });
+};
+
+// Get Own User Info
+exports.getAuthenticatedUser = (req, res) => {
+    let userData = {};
+
+    db.doc(`/users/${req.user.handle}`)
+        .get()
+        .then(doc => {
+            if(doc.exists){
+                userData.credentials = doc.data();
+                return db.collection('likes').where('userHandle', '==', req.user.handle).get();
+            }
+        })
+        .then(data => {
+            userData.likes = [];
+            data.forEach(doc => {
+                userData.likes.push(doc.data());
+            });
+            return res.json(userData);
+        })
+        .catch(err => {
+            console.error(err);
+            return res.status(500).json({ error: err.code });
+        });
+};
+
+// Upload User Img Profile
+exports.uploadImage = (req, res) => {
+    const BusBoy = require('busboy');
+    const path = require('path');
+    const os = require('os');
+    const fs = require('fs');
+
+    const busboy = new BusBoy({ headers: req.headers });
+
+    let imageFileName = '';
+    let imageToBeUploaded = {};
+
+    busboy.on('file', (fieldname, file, filename, encoding, mimetype) => {
+        console.log(fieldname);
+        console.log(filename);
+        console.log(mimetype);
+
+        if(mimetype !== 'image/jpeg' && mimetype !== 'image/png'){
+            return res.status(400).json({ error: "Wrong file type submitted!" });
+        }
+
+        // my.image.png
+        const imageExtension = filename.split('.')[filename.split('.').length - 1];
+        // 567895678956785.png
+        imageFileName = `${Math.round(Math.random()*1000000000000)}.${imageExtension}`;
+        const filepath = path.join(os.tmpdir(), imageFileName);
+
+        imageToBeUploaded = {
+            filepath,
+            mimetype
+        };
+
+        file.pipe(fs.createWriteStream(filepath));
+    });
+    busboy.on('finish', () => {
+        admin
+            .storage()
+            .bucket()
+            .upload(imageToBeUploaded.filepath, {
+            resumable: false,
+            metadata: {
+                metadata: {
+                    contentType: imageToBeUploaded.mimetype
+                }
+            }
+        })
+        .then(() => {            
+            const imageUrl = `https://firebasestorage.googleapis.com/v0/b/${config.storageBucket}/o/${imageFileName}?alt=media`;
+            return db.doc(`/users/${req.user.handle}`).update({ imageUrl });
+        })
+        .then(() => {
+            return res.json({ message: 'Image uploaded Successfully' });
+        })
+        .catch(err => {
+            console.error(err);
+            return res.status(500).json({ error: 'Something went wrong'});
+        });
+    });
+
+    busboy.end(req.rawBody);
 };
--- functions/index.js
+++ functions/index.js
@@ -8,13 +8,23 @@ const app = require('express')();
 const FBAuth = require('./util/fbAuth');
 
 const { getAllScreams, postOneScream } = require('./handlers/screams');
-const { signup, login } = require('./handlers/users');
+const { 
+    signup,
+    login,
+    uploadImage, 
+    addUserDetails, 
+    getAuthenticatedUser 
+} = require('./handlers/users');
 
 // Scream Routes
-app.get('/screams', getAllScreams);         // Get All Screams
-app.post('/scream', FBAuth, postOneScream); // Post One Scream
+app.get('/screams', getAllScreams);             // Get All Screams
+app.post('/scream', FBAuth, postOneScream);     // Post One Scream
+app.post('/user/image', FBAuth, uploadImage);   // Upload Image
+app.post('/user', FBAuth, addUserDetails);      // Edit User Info
+app.get('/user', FBAuth, getAuthenticatedUser);
+
 // User Routes
-app.post('/signup', signup);                // Signup 
-app.post('/login', login);                  // Login
+app.post('/signup', signup);                    // Signup 
+app.post('/login', login);                      // Login
 
 exports.api = functions.https.onRequest(app);
