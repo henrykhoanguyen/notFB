cdcea94430a4aa1017f396775b8edd7ea143d6be
Khoa Nguyen <henrykhoanguyen@gmail.com>
2019-08-02 11:53:00 -0700

    at 8hr mark, working on Like and Unlike Actions

--- not_facebook_client/src/pages/home.js
+++ not_facebook_client/src/pages/home.js
@@ -22,14 +22,14 @@ class home extends Component {
   }
   render() {
     let recentScreamsMarkup = this.state.screams ? (
-      this.state.screams.map(scream => (
+      this.state.screams.map((scream) => (
         <Scream key={scream.screamId} scream={scream} />
       ))
     ) : (
       <p>Loading...</p>
     );
     return (
-      <Grid container spacing={16}>
+      <Grid container spacing={5}>
         <Grid item sm={8} xs={12}>
           {recentScreamsMarkup}
         </Grid>
--- not_facebook_client/src/redux/reducers/userReducer.js
+++ not_facebook_client/src/redux/reducers/userReducer.js
@@ -2,7 +2,6 @@ import {
   SET_USER,
   SET_AUTHENTICATED,
   SET_UNAUTHENTICATED,
-  LOADING_UI,
   LOADING_USER
 } from "../types";
 
@@ -10,8 +9,8 @@ const initialState = {
   authenticated: false,
   loading: false,
   credentials: {},
-  like: [],
-  notification: []
+  likes: [],
+  notifications: []
 };
 
 export default function(state = initialState, action) {
@@ -21,19 +20,22 @@ export default function(state = initialState, action) {
         ...state,
         authenticated: true
       };
+
     case SET_UNAUTHENTICATED:
       return initialState;
+
     case SET_USER:
       return {
         authenticated: true,
         loading: false,
         ...action.payload
       };
+
     case LOADING_USER:
       return{
         ...state,
         loading: true
-      }
+      };
     default:
       return state;
   }
